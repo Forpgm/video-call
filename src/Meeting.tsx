6cac61a4e1c02400cd3fba445f84026e259623bc
Forpgm <phm.giamy@gmail.com>
2025-05-19 03:36:03 +0700

    share

--- src/Meeting.tsx
+++ src/Meeting.tsx
@@ -1,21 +1,44 @@
-import { useState } from "react";
-import AgoraRTC, { AgoraRTCProvider } from "agora-rtc-react";
+import { useEffect, useState } from "react";
+import AgoraRTC, {
+  AgoraRTCProvider,
+  type IAgoraRTCClient,
+} from "agora-rtc-react";
 import { APP_ID, CHANNEL_NAME, TOKEN } from "./constants";
 import { Videos } from "./Videos";
 
 export default function Meeting() {
-  const client = AgoraRTC.createClient({ codec: "vp8", mode: "rtc" });
+  const [videoClient] = useState<IAgoraRTCClient>(() =>
+    AgoraRTC.createClient({ mode: "rtc", codec: "vp8" })
+  );
+  const [inCall, setInCall] = useState(true);
 
-  const [inCall, setInCall] = useState<boolean>(true);
+  useEffect(() => {
+    const startCall = async () => {
+      try {
+        // Join client and publish mic+cam tracks
+        await videoClient.join(APP_ID, CHANNEL_NAME, TOKEN, null);
+        const [audioTrack, videoTrack] =
+          await AgoraRTC.createMicrophoneAndCameraTracks();
+        await videoClient.publish([audioTrack, videoTrack]);
+      } catch (err) {
+        console.error("Failed to start call:", err);
+      }
+    };
+
+    startCall();
+
+    return () => {
+      videoClient.leave();
+    };
+  }, [videoClient]);
 
   return (
     <div>
-      <AgoraRTCProvider client={client}>
+      <AgoraRTCProvider client={videoClient}>
         {inCall ? (
           <Videos
-            channelName={CHANNEL_NAME}
-            AppID={APP_ID}
-            token={TOKEN}
+            videoClient={videoClient}
+            screenClient={videoClient} // Đổi thành videoClient luôn
             onEndCall={() => setInCall(false)}
           />
         ) : (
